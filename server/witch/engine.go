55ac08a98eeeb9b0b58389c07927f2f29be69b5b
vilmibm <vilmibm@github.com>
2023-01-18 00:58:33 -0800

    refactoring

---
+++ server/witch/engine.go
@@ -0,0 +1,134 @@
+package witch
+
+import (
+	"log"
+
+	"github.com/vilmibm/hermeticum/server/db"
+	lua "github.com/yuin/gopher-lua"
+)
+
+func setHandler(l *lua.LState, verb, pattern string, cb *lua.LFunction) int {
+	handlers := l.GetGlobal("_handlers").(*lua.LTable)
+
+	verbHandlers, ok := handlers.RawGetString(verb).(*lua.LTable)
+	if !ok {
+		verbHandlers = l.NewTable()
+		handlers.RawSetString(verb, verbHandlers)
+	}
+
+	verbHandlers.RawSetString(pattern, cb)
+
+	return 0
+}
+
+func addHandler(l *lua.LState, verb string) int {
+	pattern := ".*"
+	cb := l.ToFunction(1)
+
+	return setHandler(l, verb, pattern, cb)
+}
+
+func addPatternHandler(l *lua.LState, verb string) int {
+	pattern := l.ToString(1)
+	cb := l.ToFunction(2)
+
+	return setHandler(l, verb, pattern, cb)
+}
+
+func newWitchEngine(sapi ScriptAPI, obj db.Object) (*lua.LState, error) {
+	var err error
+
+	l := lua.NewState()
+
+	l.SetGlobal("_handlers", l.NewTable())
+
+	l.SetGlobal("has", l.NewFunction(func(l *lua.LState) int {
+		l.SetGlobal("_has", l.ToTable(1))
+		return 0
+	}))
+
+	l.SetGlobal("hears", l.NewFunction(func(l *lua.LState) int {
+		return addPatternHandler(l, "say")
+	}))
+
+	l.SetGlobal("sees", l.NewFunction(func(l *lua.LState) int {
+		return addPatternHandler(l, "emote")
+	}))
+
+	l.SetGlobal("seen", l.NewFunction(func(l *lua.LState) int {
+		return addHandler(l, "look")
+	}))
+
+	l.SetGlobal("my", l.NewFunction(func(l *lua.LState) int {
+		hasT := l.GetGlobal("_has").(*lua.LTable)
+		val := hasT.RawGetString(l.ToString(1))
+		l.Push(val)
+		return 1
+	}))
+
+	l.SetGlobal("tellMe", l.NewFunction(func(l *lua.LState) int {
+		sender := l.GetGlobal("sender").(*lua.LTable)
+		senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
+
+		log.Printf("tellMe: %d %s", senderID, l.ToString(1))
+		sapi.Tell(senderID, obj.ID, l.ToString(1))
+		return 0
+	}))
+
+	l.SetGlobal("showMe", l.NewFunction(func(l *lua.LState) int {
+		sender := l.GetGlobal("sender").(*lua.LTable)
+		senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
+
+		log.Printf("showMe: %d %s", senderID, l.ToString(1))
+		sapi.Show(senderID, obj.ID, l.ToString(1))
+		return 0
+	}))
+
+	l.SetGlobal("tellSender", l.NewFunction(func(l *lua.LState) int {
+		sender := l.GetGlobal("sender").(*lua.LTable)
+		senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
+
+		log.Printf("tellMe: %d %s", senderID, l.ToString(1))
+		sapi.Tell(obj.ID, senderID, l.ToString(1))
+		return 0
+	}))
+
+	l.SetGlobal("moveSender", l.NewFunction(func(l *lua.LState) (ret int) {
+		ret = 0
+		sender := l.GetGlobal("sender").(*lua.LTable)
+		senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
+		owner := l.ToString(1)
+		name := l.ToString(2)
+		db := sapi.DB()
+		senderObj, err := db.GetObjectByID(senderID)
+		if err != nil {
+			log.Println(err.Error())
+			return
+		}
+		container, err := db.GetObject(owner, name)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err = db.MoveInto(*senderObj, *container); err != nil {
+			log.Println(err)
+		}
+
+		return
+	}))
+
+	l.SetGlobal("go", l.NewFunction(func(l *lua.LState) int {
+		// TODO get whether it's currently an exit
+		// TODO get the handler map
+		// - check if handler map has a Go handler already, exit early if so
+		// TODO register this object as an exit in DB
+		return addPatternHandler(l, "go")
+	}))
+
+	// TODO does (will need to be able to send commands to server)
+	// TODO provides
+	// TODO allows
+	// TODO showSender?
+
+	return l, l.DoString(obj.Script)
+}
--- server/witch/witch.go
+++ server/witch/witch.go
@@ -48,114 +48,66 @@ func NewScriptContext(sAPI ScriptAPI) (*ScriptContext, error) {
 	}
 	sc.incoming = make(chan VerbContext)
 
-	go func() {
-		var l *lua.LState
-		var err error
-		var vc VerbContext
-		for {
-			vc = <-sc.incoming
-			if vc.Target.Script != sc.script {
-				// TODO clear this object out of the exits table
-				sc.script = vc.Target.Script
-				l = lua.NewState()
-				l.SetGlobal("has", l.NewFunction(witchHas))
-				l.SetGlobal("hears", l.NewFunction(witchHears))
-				l.SetGlobal("sees", l.NewFunction(witchSees))
-				l.SetGlobal("go", l.NewFunction(witchGo))
-				l.SetGlobal("seen", l.NewFunction(witchSeen))
-				l.SetGlobal("my", l.NewFunction(witchMy))
-				l.SetGlobal("_handlers", l.NewTable())
-				if err := l.DoString(vc.Target.Script); err != nil {
-					log.Printf("error parsing script %s: %s", vc.Target.Script, err.Error())
-				}
-			}
-
-			l.SetGlobal("tellMe", l.NewFunction(func(l *lua.LState) int {
-				sender := l.GetGlobal("sender").(*lua.LTable)
-				senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
-
-				log.Printf("tellMe: %d %s", senderID, l.ToString(1))
-				sc.scriptAPI.Tell(senderID, vc.Target.ID, l.ToString(1))
-				return 0
-			}))
-
-			l.SetGlobal("tellSender", l.NewFunction(func(l *lua.LState) int {
-				sender := l.GetGlobal("sender").(*lua.LTable)
-				senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
-
-				log.Printf("tellMe: %d %s", senderID, l.ToString(1))
-				sc.scriptAPI.Tell(vc.Target.ID, senderID, l.ToString(1))
-				return 0
-			}))
-
-			l.SetGlobal("moveSender", l.NewFunction(func(l *lua.LState) (ret int) {
-				ret = 0
-				sender := l.GetGlobal("sender").(*lua.LTable)
-				senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
-				owner := l.ToString(1)
-				name := l.ToString(2)
-				db := sc.scriptAPI.DB()
-				senderObj, err := db.GetObjectByID(senderID)
-				if err != nil {
-					log.Println(err.Error())
-					return
-				}
-				container, err := db.GetObject(owner, name)
-				if err != nil {
-					log.Println(err.Error())
-					return
-				}
-				if err = db.MoveInto(*senderObj, *container); err != nil {
-					log.Println(err.Error())
-				}
-
-				return
-			}))
-
-			l.SetGlobal("showMe", l.NewFunction(func(l *lua.LState) int {
-				sender := l.GetGlobal("sender").(*lua.LTable)
-				senderID := int(lua.LVAsNumber(sender.RawGetString("ID")))
-
-				log.Printf("showMe: %d %s", senderID, l.ToString(1))
-				sc.scriptAPI.Show(senderID, vc.Target.ID, l.ToString(1))
-				return 0
-			}))
-
-			// TODO showSender?
-
-			// TODO check execute permission and bail out potentially
-			//log.Printf("%#v", vc)
-
-			senderT := l.NewTable()
-			senderT.RawSetString("name", lua.LString(vc.Sender.Data["name"]))
-			senderT.RawSetString("ID", lua.LNumber(vc.Sender.ID))
-			l.SetGlobal("sender", senderT)
-			l.SetGlobal("msg", lua.LString(vc.Rest))
-
-			handlers := l.GetGlobal("_handlers").(*lua.LTable)
-			handlers.ForEach(func(k, v lua.LValue) {
-				if k.String() != vc.Verb {
-					return
-				}
-				v.(*lua.LTable).ForEach(func(kk, vv lua.LValue) {
-					pattern := regexp.MustCompile(kk.String())
-					if pattern.MatchString(vc.Rest) {
-						// TODO TODO TODO TODO TODO
-						// this could be a remote code execution vuln; but by being here, I
-						// believe vc.Verb has been effectively validated as "not a pile of
-						// lua code" since it matched a handler.
-						if err = l.DoString(fmt.Sprintf(`_handlers.%s["%s"]()`, vc.Verb, pattern)); err != nil {
-							log.Println(err.Error())
-						}
-					}
-				})
-			})
-		}
-	}()
+	go sc.Go()
 
 	return sc, nil
 }
 
 func (sc *ScriptContext) Handle(vc VerbContext) {
 	sc.incoming <- vc
+}
+
+func (sc *ScriptContext) Go() {
+	var (
+		l           *lua.LState
+		err         error
+		vc          VerbContext
+		senderT     *lua.LTable
+		handlers    *lua.LTable
+		pattern     *regexp.Regexp
+		handlerCall string
+	)
+
+	for {
+		vc = <-sc.incoming
+		if vc.Target.Script != sc.script {
+			sc.script = vc.Target.Script
+			l, err = newWitchEngine(sc.scriptAPI, vc.Target)
+			if err != nil {
+				log.Printf("error parsing script %s: %s", sc.script, err.Error())
+				continue
+			}
+
+			// TODO clear this object out of the exits table
+		}
+
+		// TODO check execute permission and bail out potentially
+
+		senderT = l.NewTable()
+		senderT.RawSetString("name", lua.LString(vc.Sender.Data["name"]))
+		senderT.RawSetString("ID", lua.LNumber(vc.Sender.ID))
+		l.SetGlobal("sender", senderT)
+		l.SetGlobal("msg", lua.LString(vc.Rest))
+
+		handlers = l.GetGlobal("_handlers").(*lua.LTable)
+		handlers.ForEach(func(k, v lua.LValue) {
+			if k.String() != vc.Verb {
+				return
+			}
+			v.(*lua.LTable).ForEach(func(kk, vv lua.LValue) {
+				pattern = regexp.MustCompile(kk.String())
+				if !pattern.MatchString(vc.Rest) {
+					return
+				}
+				// TODO TODO TODO TODO TODO
+				// this could be a remote code execution vuln; but by being here, I
+				// believe vc.Verb has been effectively validated as "not a pile of
+				// lua code" since it matched a handler.
+				handlerCall = fmt.Sprintf(`_handlers.%s["%s"]()`, vc.Verb, pattern)
+				if err = l.DoString(handlerCall); err != nil {
+					log.Println(err)
+				}
+			})
+		})
+	}
 }
