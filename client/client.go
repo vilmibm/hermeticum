e4e11a5bd7cf215b7489efadba760988f277a024
nate smith <nateksmith@pm.me>
2024-12-07 12:57:57 -0600

    heinous wip to try out bubbletea

--- client/client.go
+++ client/client.go
@@ -4,29 +4,109 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
-	"os/signal"
+	"os/exec"
+	"strconv"
 	"strings"
 	"text/template"
 	"time"
 
-	"github.com/gdamore/tcell/v2"
-	"github.com/rivo/tview"
+	"github.com/charmbracelet/bubbles/cursor"
+	"github.com/charmbracelet/bubbles/textarea"
+	"github.com/charmbracelet/bubbles/viewport"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/vilmibm/hermeticum/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+type model struct {
+	prompt   textarea.Model
+	events   []*proto.WorldEvent
+	room     *proto.Object
+	contents []*proto.Object
+	viewport viewport.Model
+}
+
+func initialModel() model {
+	prompt := textarea.New()
+	prompt.Placeholder = "lol"
+	prompt.Focus()
+	prompt.Prompt = "> "
+	prompt.SetWidth(80)
+	prompt.SetHeight(3)
+	prompt.FocusedStyle.CursorLine = lipgloss.NewStyle()
+
+	prompt.ShowLineNumbers = false
+
+	vp := viewport.New(80, 20)
+	vp.SetContent("^_^")
+	prompt.KeyMap.InsertNewline.SetEnabled(false)
+
+	return model{
+		prompt:   prompt,
+		viewport: vp,
+		events:   []*proto.WorldEvent{},
+		room:     nil,
+		contents: []*proto.Object{},
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return textarea.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		m.prompt.SetWidth(msg.Width)
+		return m, nil
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			v := m.prompt.Value()
+			if v == "" {
+				return m, nil
+			}
+			// TODO custom command to server send
+			return m, nil
+		default:
+			var cmd tea.Cmd
+			m.prompt, cmd = m.prompt.Update(msg)
+			return m, cmd
+		}
+	case cursor.BlinkMsg:
+		var cmd tea.Cmd
+		m.prompt, cmd = m.prompt.Update(msg)
+		return m, cmd
+	default:
+		return m, nil
+	}
+}
+
+func (m model) View() string {
+	return fmt.Sprintf(
+		"%s\n\n%s",
+		m.viewport.View(),
+		m.prompt.View(),
+	) + "\n\n"
+}
+
 type ConnectOpts struct {
 }
 
 type ClientState struct {
-	App          *tview.Application
-	details      *tview.TextView
-	Client       proto.GameWorldClient
-	MaxMessages  int
-	messagesView *tview.TextView
+	//App          *tview.Application
+	//details      *tview.TextView
+	Client      proto.GameWorldClient
+	MaxMessages int
+	//messagesView *tview.TextView
 	events       []*proto.WorldEvent
 	cio          *clientIO
 	currentRoom  *proto.Object
@@ -81,9 +161,9 @@ func (cs *ClientState) handleStateUpdate(ev *proto.WorldEvent) {
 		cs.logger.Printf("failed to render details template: %s", err.Error())
 	}
 	cs.roomContents = ev.GetObjects()
-	cs.App.QueueUpdateDraw(func() {
-		cs.details.SetText(update.String())
-	})
+	//cs.App.QueueUpdateDraw(func() {
+	//	cs.details.SetText(update.String())
+	//})
 }
 
 func (cs *ClientState) AddMessage(ev *proto.WorldEvent) {
@@ -94,20 +174,20 @@ func (cs *ClientState) AddMessage(ev *proto.WorldEvent) {
 	}
 
 	// TODO look into using the SetChangedFunc thing.
-	cs.App.QueueUpdateDraw(func() {
-		// TODO trim content of messagesView /or/ see if tview has a buffer size that does it for me. use cs.messages to re-constitute.
-		switch ev.Type {
-		case proto.WorldEvent_OVERHEARD:
-			fmt.Fprintf(cs.messagesView, "%s: %s\n", ev.GetSource(), ev.GetText())
-		case proto.WorldEvent_EMOTE:
-			fmt.Fprintf(cs.messagesView, "%s %s\n", ev.GetSource(), ev.GetText())
-		case proto.WorldEvent_PRINT:
-			fmt.Fprintf(cs.messagesView, "%s\n", ev.GetText())
-		default:
-			fmt.Fprintf(cs.messagesView, "%#v\n", ev)
-		}
-		cs.messagesView.ScrollToEnd()
-	})
+	//cs.App.QueueUpdateDraw(func() {
+	//	// TODO trim content of messagesView /or/ see if tview has a buffer size that does it for me. use cs.messages to re-constitute.
+	//	switch ev.Type {
+	//	case proto.WorldEvent_OVERHEARD:
+	//		fmt.Fprintf(cs.messagesView, "%s: %s\n", ev.GetSource(), ev.GetText())
+	//	case proto.WorldEvent_EMOTE:
+	//		fmt.Fprintf(cs.messagesView, "%s %s\n", ev.GetSource(), ev.GetText())
+	//	case proto.WorldEvent_PRINT:
+	//		fmt.Fprintf(cs.messagesView, "%s\n", ev.GetText())
+	//	default:
+	//		fmt.Fprintf(cs.messagesView, "%#v\n", ev)
+	//	}
+	//	cs.messagesView.ScrollToEnd()
+	//})
 	/*
 		for _, ev := range cs.events {
 			fmt.Print("\x1b[1B")
@@ -138,7 +218,7 @@ func Connect(opts ConnectOpts) error {
 		return err
 	}
 	client := proto.NewGameWorldClient(gc)
-	app := tview.NewApplication()
+	//app := tview.NewApplication()
 
 	cio := &clientIO{
 		inbound:  make(chan *proto.WorldEvent),
@@ -150,7 +230,7 @@ func Connect(opts ConnectOpts) error {
 	// TODO make a NewClientState
 	// TODO rename this, like, UI
 	cs := &ClientState{
-		App:         app,
+		//App:         app,
 		Client:      client,
 		MaxMessages: 15, // TODO for testing
 		events:      []*proto.WorldEvent{},
@@ -164,47 +244,47 @@ func Connect(opts ConnectOpts) error {
 		log.Fatalf("%v.Ping -> %v", cs.Client, err)
 	}
 
-	commandInput := tview.NewInputField().SetLabel("> ")
-	handleInput := func(_ tcell.Key) {
-		input := commandInput.GetText()
-		// TODO command history
-		commandInput.SetText("")
-		// TODO do i need to clear the input's text?
-		cs.HandleInput(input)
-	}
+	//commandInput := tview.NewInputField().SetLabel("> ")
+	//handleInput := func(_ tcell.Key) {
+	//	input := commandInput.GetText()
+	//	// TODO command history
+	//	commandInput.SetText("")
+	//	// TODO do i need to clear the input's text?
+	//	cs.HandleInput(input)
+	//}
 
-	commandInput.SetDoneFunc(handleInput)
+	//commandInput.SetDoneFunc(handleInput)
 
 	// TODO need to hit ctrl c twice to quit but otherwise quitting works how i want
-	sigC := make(chan os.Signal, 1)
-	signal.Notify(sigC, os.Interrupt)
+	//sigC := make(chan os.Signal, 1)
+	//signal.Notify(sigC, os.Interrupt)
 
-	msgView := tview.NewTextView().SetScrollable(true).SetWrap(true).SetWordWrap(true)
-	cs.messagesView = msgView
-	cs.details = tview.NewTextView().SetText("...")
+	//msgView := tview.NewTextView().SetScrollable(true).SetWrap(true).SetWordWrap(true)
+	//cs.messagesView = msgView
+	//cs.details = tview.NewTextView().SetText("...")
 
-	gamePage := tview.NewGrid().
-		SetRows(1, 40, 3).
-		SetColumns(-1, -1).
-		SetBorders(true).
-		AddItem(
-			tview.NewTextView().SetTextAlign(tview.AlignLeft).SetText("h e r m e t i c u m"),
-			0, 0, 1, 1, 1, 1, false).
-		AddItem(
-			tview.NewTextView().SetTextAlign(tview.AlignRight).SetText("TODO server status"),
-			0, 1, 1, 1, 1, 1, false).
-		AddItem(
-			msgView,
-			1, 0, 1, 1, 10, 20, false).
-		AddItem(
-			cs.details,
-			1, 1, 1, 1, 10, 10, false).
-		AddItem(
-			commandInput,
-			2, 0, 1, 2, 1, 30, false)
+	//gamePage := tview.NewGrid().
+	//	SetRows(1, 40, 3).
+	//	SetColumns(-1, -1).
+	//	SetBorders(true).
+	//	AddItem(
+	//		tview.NewTextView().SetTextAlign(tview.AlignLeft).SetText("h e r m e t i c u m"),
+	//		0, 0, 1, 1, 1, 1, false).
+	//	AddItem(
+	//		tview.NewTextView().SetTextAlign(tview.AlignRight).SetText("TODO server status"),
+	//		0, 1, 1, 1, 1, 1, false).
+	//	AddItem(
+	//		msgView,
+	//		1, 0, 1, 1, 10, 20, false).
+	//	AddItem(
+	//		cs.details,
+	//		1, 1, 1, 1, 10, 10, false).
+	//	AddItem(
+	//		commandInput,
+	//		2, 0, 1, 2, 1, 30, false)
 
-	pages := tview.NewPages()
-	pages.AddPage("game", gamePage, true, true)
+	//pages := tview.NewPages()
+	//pages.AddPage("game", gamePage, true, true)
 
 	ctx := context.Background()
 
@@ -224,13 +304,13 @@ func Connect(opts ConnectOpts) error {
 		}
 	}()
 
-	go func() {
-		err := app.SetRoot(pages, true).SetFocus(commandInput).Run()
-		if err != nil {
-			cio.errs <- err
-			cio.done <- true
-		}
-	}()
+	//go func() {
+	//	err := app.SetRoot(pages, true).SetFocus(commandInput).Run()
+	//	if err != nil {
+	//		cio.errs <- err
+	//		cio.done <- true
+	//	}
+	//}()
 
 	/*
 		go func() {
@@ -249,43 +329,120 @@ func Connect(opts ConnectOpts) error {
 		}()
 	*/
 
+	//go func() {
+	//	for range sigC {
+	//		cmd := &proto.Command{
+	//			Verb: "quit",
+	//		}
+	//		cs.cio.outbound <- cmd
+	//	}
+	//}()
 	go func() {
-		for range sigC {
-			cmd := &proto.Command{
-				Verb: "quit",
+		for {
+			select {
+			case ev := <-cio.inbound:
+				cs.handleInbound(ev)
+			case cmd := <-cio.outbound:
+				if err := stream.Send(cmd); err != nil {
+					cio.errs <- err
+				}
+				if cmd.Verb == "quit" {
+					cio.done <- true
+				}
+				if cmd.Verb == "edit" {
+					var o *proto.Object
+					id, err := strconv.Atoi(cmd.Rest)
+					if err == nil {
+						o = resolveObjectById(cs.roomContents, id)
+					}
+
+					if o == nil {
+						matches := resolveObjectByString(cs.roomContents, cmd.Rest)
+						if len(matches) == 1 {
+							o = matches[0]
+						} else if len(matches) > 1 {
+							// TODO fuzzy error
+							panic("non unique item")
+						}
+					}
+
+					if o == nil {
+						// TODO no such object error
+						panic("no such dingus, dingus")
+					}
+
+					// TODO lock object
+
+					editor := "/usr/bin/vim"
+					if v := os.Getenv("VISUAL"); v != "" {
+						editor = v
+					} else if e := os.Getenv("EDITOR"); e != "" {
+						editor = e
+					}
+
+					f, err := os.CreateTemp("", fmt.Sprintf("hermeticum-%s-*.lua", o.GetName()))
+					if err != nil {
+						// TODO
+						panic(err)
+					}
+					// TODO as expected, this fails catastrophically.
+					// TODO I'm considering switching to bubbletea, anyway. so i might give
+					// up on external editors for now and just use their multi line editing
+					// thing.
+					cmd := exec.Command(editor, f.Name())
+					cmd.Stdin = os.Stdin
+					cmd.Stdout = os.Stdout
+					err = cmd.Run()
+					if err != nil {
+						// TODO
+						panic(err)
+					}
+
+					newContent, err := io.ReadAll(f)
+					if err != nil {
+						// TODO
+						panic(err)
+					}
+					f.Close()
+
+					cs.logger.Println(newContent)
+
+					// TODO update object
+
+					// TODO unlock object
+
+				}
+			case err := <-cio.errs:
+				log.Printf("error: %s", err.Error())
+			case <-cio.done:
+				// TODO this triggers data race warning when run with -race
+				//cs.App.Stop() // TODO should this be in a defer
+				//return nil
 			}
-			cs.cio.outbound <- cmd
 		}
 	}()
 
-	for {
-		select {
-		case ev := <-cio.inbound:
-			cs.handleInbound(ev)
-		case cmd := <-cio.outbound:
-			if err := stream.Send(cmd); err != nil {
-				cio.errs <- err
-			}
-			if cmd.Verb == "quit" {
-				cio.done <- true
-			}
-			if cmd.Verb == "edit" {
-				// TODO need to resolve object and get its script. right now the client
-				// is dumb -- it just sends and receives strings. this is what
-				// motivated the idea of client state events.
-				//
-				// if the client maintains a list of every object in the current room
-				// that is updated when state messages are received, edit will always
-				// have the latest data for objects.
-				//
-				// I'll need a place in RAM to store "room contents"
-			}
-		case err := <-cio.errs:
-			log.Printf("error: %s", err.Error())
-		case <-cio.done:
-			// TODO this triggers data race warning when run with -race
-			cs.App.Stop()
-			return nil
+	p := tea.NewProgram(initialModel())
+	_, err = p.Run()
+	return err
+}
+
+func resolveObjectByString(objs []*proto.Object, s string) []*proto.Object {
+	found := []*proto.Object{}
+	for _, o := range objs {
+		if strings.Contains(o.GetName(), s) {
+			found = append(found, o)
 		}
 	}
+	return found
+}
+
+func resolveObjectById(objs []*proto.Object, id int) *proto.Object {
+	for _, o := range objs {
+		if o.Id == uint64(id) {
+			return o
+		}
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -7,12 +7,17 @@ require (
 	github.com/jackc/pgx/v5 v5.5.5
 	github.com/rivo/tview v0.0.0-20220703182358-a13d901d3386
 	github.com/spf13/cobra v1.8.1
-	golang.org/x/sys v0.25.0
+	golang.org/x/sys v0.27.0
 	google.golang.org/grpc v1.66.2
 	google.golang.org/protobuf v1.34.2
 )
 
 require (
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/x/ansi v0.4.5 // indirect
+	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -20,15 +25,25 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/mattn/go-runewidth v0.0.13 // indirect
-	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.27.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
 	golang.org/x/term v0.24.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
 )
 
-require github.com/yuin/gopher-lua v0.0.0-20221210110428-332342483e3f
+require (
+	github.com/charmbracelet/bubbles v0.20.0
+	github.com/charmbracelet/bubbletea v1.2.4
+	github.com/charmbracelet/lipgloss v1.0.0
+	github.com/yuin/gopher-lua v0.0.0-20221210110428-332342483e3f
+)
